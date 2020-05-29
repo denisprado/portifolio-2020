829b9804b157eef5d7e57166131330f9edc22a74
denisforigo <denisforigo@gmail.com>
2020-05-29 15:42:32 -0300

    Admin

--- src/components/slider_/slide.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Container } from './slide.css'
-
-const Slide = ({ content, width }) => (
-    <Container content={content} width={width} />
-)
-
-export default Slide
