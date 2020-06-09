35cd91a069317c7e9f11a88d156f9fd43a3ac333
denisforigo <denisforigo@gmail.com>
2020-06-08 21:16:12 -0300

    working in PageCreation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+const _ = require('lodash')
+const { createFilePath } = require('gatsby-source-filesystem')
+const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 exports.onCreateWebpackConfig = ({
-  stage,
-  getConfig,
-  rules,
-  loaders,
+  stage, loaders,
   actions,
 }) => {
   actions.setWebpackConfig({
@@ -20,8 +20,85 @@ exports.onCreateWebpackConfig = ({
   });
 };
 
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
 
-exports.onCreateNode = ({ node }) => {
-  fmImagesToRelative(node);
-};
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000, filter: {frontmatter: {collection: {eq: "work"}}}) {
+        nodes {
+          fields {
+            slug
+          }
+          id
+          frontmatter {
+            client
+            collection
+            discipline
+          }
+        }
+      }
+    }
+  `).then((result) => {
+    if (result.errors) {
+      result.errors.forEach((e) => console.error(e.toString()))
+      // eslint-disable-next-line no-undef
+      return Promise.reject(result.errors)
+    }
+
+    const posts = result.data.allMarkdownRemark.nodes
+    posts.forEach((edge) => {
+      console.log(edge.fields.slug);
+      const id = edge.id;
+      createPage({
+        path: edge.fields.slug,
+        discipline: edge.frontmatter.discipline,
+        component: path.resolve(
+          `src/templates/${String(edge.frontmatter.collection)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          id,
+        },
+      })
+    })
+
+    // Tag pages:
+    let discipline = []
+    // Iterate through each post, putting all found discipline into `discipline`
+    posts.forEach((edge) => {
+      if (_.get(edge, 'frontmatter.discipline')) {
+        discipline = discipline.concat(edge.frontmatter.discipline)
+      }
+    })
+    // Eliminate duplicate discipline
+    discipline = _.uniq(discipline)
+
+    // Make tag pages
+    discipline.forEach((tag) => {
+      const tagPath = `/discipline/${_.kebabCase(tag)}/`
+
+      createPage({
+        path: tagPath,
+        component: path.resolve('src/templates/discipline.js'),
+        context: {
+          tag,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  fmImagesToRelative(node) // convert image paths for gatsby images
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: 'slug',
+      node,
+      value,
+    })
+  }
+}
--- src/components/slider/slide/slide.js
+++ src/components/slider/slide/slide.js
@@ -1,18 +1,27 @@
 /** @jsx jsx */
 import React from 'react'
 import { css, jsx } from '@emotion/core'
+import Title from 'components/title';
 
 const Slide = ({ content, width }) => (
-  <div
-    css={css`
+  <>
+    <div
+      css={css`
       height: 100%;
       width: ${width}px;
-      background-image: url('${content}');
+      background-image: url('${content.slide.childImageSharp.fluid.src}');
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
     `}
-  />
+    >
+    </div>
+    <Title css={css`
+      width: 100px;
+      bottom: 2rem;
+      position: absolute;
+    `}>{content.title}</Title>
+  </>
 )
 
 export default Slide
